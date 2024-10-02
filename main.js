3f14be365eb13ab3ae94c8c34f9dc397f86a124b
Shahab Ali <shahabalisa08@gmail.com>
2024-10-02 15:12:26 +0500

    add

---
+++ main.js
@@ -0,0 +1,10 @@
+var number = prompt("Guess the number 1 to 20");
+if (number === "7"){
+    alert("Congrats! Your guess is right");
+}
+else if ( number === "6"){
+    alert("you are very close to guess the right number");
+}
+else{
+    alert("Wrong");
+}
